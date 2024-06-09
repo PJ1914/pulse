8baed9c674182215332ebc52bfe596e562b58c48
Vishwa <vishwateja2684@gmail.com>
2024-06-09 15:56:43 +0530

    Should include user auth but i currently created UI for chat interface

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import PulseAni from './components/PulseAni';
 import Main from './components/Main';
-import Messages from './components/Messages';
+import Messages from './components/messages/Messages';
 import Weather from './components/Weather'; // Import the Weather component
 
 function App() {
@@ -11,63 +11,63 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/pulseai" element={<PulseAni />} />
-                <Route path="/main" element={<MainWithAxios />} />
-                <Route path="/messages" element={<MessagesWithAxios />} />
-                <Route path="/weather" element={<WeatherWithAxios />} /> {/* Add Weather route */}
+                <Route path="/main" element={<Main />} />
+                <Route path="/messages" element={<Messages />} />
+                <Route path="/weather" element={<Weather />} /> {/* Add Weather route */}
             </Routes>
         </Router>
     );
 }
 
-function MainWithAxios() {
-    const [data, setData] = useState(null);
+// function MainWithAxios() {
+//     const [data, setData] = useState(null);
 
-    useEffect(() => {
-        axios.get('http://127.0.0.1:8000/api/main/')
-            .then(response => {
-                setData(response.data);
-            })
-            .catch(error => {
-                console.error('There was an error making the request!', error);
-            });
-    }, []);
+//     useEffect(() => {
+//         axios.get('http://127.0.0.1:8000/api/main/')
+//             .then(response => {
+//                 setData(response.data);
+//             })
+//             .catch(error => {
+//                 console.error('There was an error making the request!', error);
+//             });
+//     }, []);
 
-    return <Main data={data} />;
-}
+//     return <Main data={data} />;
+// }
 
-function MessagesWithAxios() {
-    const [data, setData] = useState(null);
+// function MessagesWithAxios() {
+//     const [data, setData] = useState(null);
 
-    useEffect(() => {
-        axios.get('http://127.0.0.1:8000/api/messages/')
-            .then(response => {
-                setData(response.data);
-            })
-            .catch(error => {
-                console.error('There was an error making the request!', error);
-            });
-    }, []);
+//     useEffect(() => {
+//         axios.get('http://127.0.0.1:8000/api/messages/')
+//             .then(response => {
+//                 setData(response.data);
+//             })
+//             .catch(error => {
+//                 console.error('There was an error making the request!', error);
+//             });
+//     }, []);
 
-    return <Messages data={data} />;
-}
+//     return <Messages data={data} />;
+// }
 
-function WeatherWithAxios() {
-    const [weatherData, setWeatherData] = useState(null);
-    const [location, setLocation] = useState(''); // State for user input
+// function WeatherWithAxios() {
+//     const [weatherData, setWeatherData] = useState(null);
+//     const [location, setLocation] = useState(''); // State for user input
 
-    useEffect(() => {
-        if (location) { // Fetch data only if location is provided
-            axios.get(`http://127.0.0.1:8000/api/weather/${location}/`)
-                .then(response => {
-                    setWeatherData(response.data);
-                })
-                .catch(error => {
-                    console.error('There was an error fetching the weather data!', error);
-                });
-        }
-    }, [location]); // Dependency on location, fetches weather data when location changes
+//     useEffect(() => {
+//         if (location) { // Fetch data only if location is provided
+//             axios.get(`http://127.0.0.1:8000/api/weather/${location}/`)
+//                 .then(response => {
+//                     setWeatherData(response.data);
+//                 })
+//                 .catch(error => {
+//                     console.error('There was an error fetching the weather data!', error);
+//                 });
+//         }
+//     }, [location]); // Dependency on location, fetches weather data when location changes
 
-    return <Weather weatherData={weatherData} setLocation={setLocation} />;
-}
+//     return <Weather weatherData={weatherData} setLocation={setLocation} />;
+// }
 
 export default App;
