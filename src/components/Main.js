416f0b5c751a58613c0e9ac3d300bfef2e48e899
Vishwa <vishwateja2684@gmail.com>
2024-06-09 16:01:28 +0530

    'chat with pulse' in main could've done neater

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,11 @@
 // src/components/Main.js
 import React from 'react';
-
+import { useNavigate,Link } from 'react-router-dom';
 const Main = () => {
     return (
         <div>
             <h1>Welcome to the Main Page</h1>
+            <h2><Link to='/messages'>chat with Pulse</Link></h2>
         </div>
     );
 };
