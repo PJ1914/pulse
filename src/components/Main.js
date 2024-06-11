51ffd0b07be5a2ff6535fb78f71c5af8806b8c1e
CHINTU PRANAY <pranay.jumbarthi1905@gmail.com>
2024-06-11 17:48:53 +0530

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,11 @@
 // src/components/Main.js
 import React from 'react';
+import styled from 'styled-components';
 import { useNavigate,Link } from 'react-router-dom';
 const Main = () => {
     return (
         <div>
-            <h1><Link to=''>Welcome to the Main Page</Link></h1>
+            <h1><Link to='/login'>Welcome to the Main Page</Link></h1>
             <h2><Link to='/messages'>chat with Pulse</Link></h2>
         </div>
 
