c26a185e5ccfa76fdd5e5eaba21903a4462992d7
Vishwa <vishwateja2684@gmail.com>
2024-06-21 20:11:12 +0530

    hid it now

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -14,8 +14,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FB_MSID,
   appId: process.env.REACT_APP_FB_AID
 };
-console.log(firebaseConfig)
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
