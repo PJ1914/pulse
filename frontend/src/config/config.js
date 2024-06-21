613dc7be9c4052f196cab468ded202f2d4bcbf2d
Vishwa <vishwateja2684@gmail.com>
2024-06-21 20:10:51 +0530

    hid firebase creds

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
+
 import {getFirestore} from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBN2DNVK9DB1irsxIwt4KceHjD0F0wmv2g",
-  authDomain: "pulseai-354b6.firebaseapp.com",
-  projectId: "pulseai-354b6",
-  storageBucket: "pulseai-354b6.appspot.com",
-  messagingSenderId: "430968379184",
-  appId: "1:430968379184:web:ed62dc39fb4236d9609af5"
+  apiKey: process.env.REACT_APP_FB_API,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PID,
+  storageBucket: process.env.REACT_APP_FB_SB,
+  messagingSenderId: process.env.REACT_APP_FB_MSID,
+  appId: process.env.REACT_APP_FB_AID
 };
+console.log(firebaseConfig)
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
