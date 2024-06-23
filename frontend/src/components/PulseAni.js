4d28f76a9ac80a068ddd9db2d6c95f72628a531b
Vishwa <vishwateja2684@gmail.com>
2024-06-23 18:40:12 +0530

    I just commented the vid tag in PulseAni and removed video temporarily (got a copy of video)

--- frontend/src/components/PulseAni.js
+++ frontend/src/components/PulseAni.js
@@ -2,15 +2,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './PulseAni.css';
-import videoBackground from '../assets/vecteezy_purple-and-white-3d-technology-element-artificial_27989661.mp4';
+// import videoBackground from '../assets/vecteezy_purple-and-white-3d-technology-element-artificial_27989661.mp4';
 
 const PulseAni = () => {
     return (
         <div className="pulse-container">
-            <video autoPlay loop muted className="pulse-background">
+            {/* <video autoPlay loop muted className="pulse-background">
                 <source src={videoBackground} type="video/mp4" />
                 Your browser does not support the video tag.
-            </video>
+            </video> */}
             <div className="pulse-content">
                 <Link to="/main" className="pulse-link">
                     <h1 className="pulse-title">PULSE AI</h1>
