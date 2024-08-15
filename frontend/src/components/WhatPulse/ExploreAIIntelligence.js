6ca134a50fcb9efcea747d1f87eb64dfe1e096fa
laxmi prasanna <prasanna51856@gmail.com>
2024-08-15 20:38:28 +0530

    exploreintelligencedone

--- frontend/src/components/WhatPulse/ExploreAIIntelligence.js
+++ frontend/src/components/WhatPulse/ExploreAIIntelligence.js
@@ -1,79 +1,20 @@
-// ExploreAIIntelligence.js
 import React from "react";
 import Icon from "./Icon";
+import "./ExploreAIIntelligence.css"; // Import the CSS file
 
 const ExploreAIIntelligence = () => {
   return (
     <>
-      <div style={{ position: "absolute", left: "5%", paddingBlockEnd: "35%" }}>
-        <span
-          style={{
-            color: "#999999",
-            fontSize: "3.5vw",
-            fontFamily: "MADE Tommy Soft",
-            fontWeight: 700,
-            lineHeight: "1.2em",
-            wordWrap: "break-word",
-          }}
-        >
-          Explore
-        </span>
-        <span
-          style={{
-            color: "white",
-            fontSize: "3.5vw",
-            fontFamily: "MADE Tommy Soft",
-            fontWeight: 700,
-            lineHeight: "1.2em",
-            wordWrap: "break-word",
-          }}
-        >
-          {" "}
-          PULSE AI
-        </span>
-        <span
-          style={{
-            color: "white",
-            fontSize: "3vw",
-            fontFamily: "MADE Tommy Soft",
-            fontWeight: 700,
-            lineHeight: "1.2em",
-            wordWrap: "break-word",
-          }}
-        >
-          {" "}
-        </span>
-        <span
-          style={{
-            color: "#999999",
-            fontSize: "3vw",
-            fontFamily: "MADE Tommy Soft",
-            fontWeight: 700,
-            lineHeight: "1.2em",
-            wordWrap: "break-word",
-          }}
-        >
+      <div className="explore-ai-container">
+        <span className="explore-ai-text gray">Explore</span>
+        <span className="explore-ai-text"> PULSE AI</span>
+        <span className="explore-ai-text" style={{ fontSize: "3vw" }}> </span>
+        <span className="explore-ai-text gray" style={{ fontSize: "3vw" }}>
           Intelligence
         </span>
       </div>
       <Icon />
-      <div
-        style={{
-          position: "absolute",
-          left: "4%",
-          marginBottom: "20%",
-          width: "94%",
-          maxWidth: "1378px",
-          color: "#E9E9E9",
-          fontSize: "2vw",
-          fontFamily: "Arial",
-          fontWeight: 700,
-          lineHeight: "1.2em",
-          wordWrap: "break-word",
-          marginTop: "2%",
-          padding: "1%",
-        }}
-      >
+      <div className="explore-ai-intro">
         Scroll down to explore the technology behind Pulse AI and meet the team
         making it happen. Along the way, you'll find our AI chatbot ready to
         assist you and a deeper look into Pulse AI Intelligence where innovation
